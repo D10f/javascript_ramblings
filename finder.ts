e4340f95bd56020f31b59b0fd2b7f0cca474acc1
herokunt <galactodensimeter_bismuthyl@simplelogin.fr>
2022-03-02 20:58:59 +0100

    prettified

--- finder.ts
+++ finder.ts
@@ -1,21 +1,22 @@
 // TODO: Add regular expression support
 // TODO: Implement breath-first search
+// TODO: Add option to filter output results (using set)
 
 enum ALGORITHMS {
   "DEPTH_FIRST_SEARCH",
   "BREATH_FIRST_SEARCH",
-};
+}
 
 interface IFinderOptions {
   algorithm?: ALGORITHMS;
   filterKeys?: string | undefined;
   filterValues?: string | undefined;
-  returnEmptyValues?: boolean,
+  returnEmptyValues?: boolean;
   maxDepth?: number | undefined;
   depthCount?: number;
-};
+}
 
-type IFinderProvided = Omit<IFinderOptions, 'depthCount'>;
+type IFinderProvided = Omit<IFinderOptions, "depthCount">;
 
 const defaultOptions: IFinderOptions = {
   algorithm: ALGORITHMS.DEPTH_FIRST_SEARCH,
@@ -28,34 +29,33 @@ const defaultOptions: IFinderOptions = {
 
 function getSettings(options: IFinderOptions): IFinderOptions {
   return { ...defaultOptions, ...options };
-};
+}
 
 function isNullOrUndefined(value: unknown): boolean {
   return value === null || value === undefined;
-};
+}
 
 function isObject(value: string): boolean {
   return !isNullOrUndefined(value) && value.constructor.name === "Object";
-};
+}
 
 function isArray(value: string): boolean {
   return value.constructor.name === "Array";
-};
+}
 
 function isAtMaxDepth(options: IFinderOptions): boolean {
   return Boolean(options.maxDepth && options.depthCount! >= options.maxDepth);
-};
+}
 
 function increaseDepthCount(options: IFinderOptions): IFinderOptions {
   return { ...options, depthCount: options.depthCount! + 1 };
-};
+}
 
 function* _findInTree(
   tree: object,
   options: IFinderProvided
 ): Generator<unknown, any, any> {
   for (const [key, value] of Object.entries(tree)) {
-
     if (isNullOrUndefined(value) && !options.returnEmptyValues) {
       continue;
     }
@@ -80,19 +80,16 @@ function* _findInTree(
 
     yield value;
   }
-};
+}
 
-function findInTree(
-  tree: object,
-  options: IFinderProvided = {}
-): any[] {
+function findInTree(tree: object, options: IFinderProvided = {}): any[] {
   const settings = getSettings(options);
   const response = [];
   for (const node of _findInTree(tree, settings)) {
     response.push(node);
   }
   return response;
-};
+}
 
 // const data = {
 //   value: 1,

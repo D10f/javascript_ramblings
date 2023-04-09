8bc678ba395920988cc637b5787f78649846c1ea
D10f <galactodensimeter_bismuthyl@simplelogin.fr>
2023-04-09 21:33:19 +0200

    Add pacman movement in fixed increments to the cell width/height

--- 2D/pacman/src/main.ts
+++ 2D/pacman/src/main.ts
@@ -1,6 +1,8 @@
 import './style.css';
 
+import { CELL_SIZE } from './defs';
 import Grid from './components/Grid';
+import Player from './components/Player';
 
 const canvas = document.getElementById('canvas1') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
@@ -9,6 +11,14 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 const grid = new Grid();
+const player = new Player({ x: CELL_SIZE * 3, y: CELL_SIZE * 3 });
 
-grid.draw(ctx);
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  grid.draw(ctx);
+  player.update();
+  player.draw(ctx);
+  window.requestAnimationFrame(animate);
+}
 
+animate();

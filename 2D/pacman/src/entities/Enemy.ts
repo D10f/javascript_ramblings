b17dac83ce1d2f2e59bf7916564d138f46b3aeec
D10f <galactodensimeter_bismuthyl@simplelogin.fr>
2023-04-12 12:40:42 +0200

    refactor enemy's method to find a random target on grid

--- 2D/pacman/src/entities/Enemy.ts
+++ 2D/pacman/src/entities/Enemy.ts
@@ -36,70 +36,37 @@ class Enemy {
             new Vector(this.position.x - CELL_SIZE, this.position.y),
         ];
 
-        while (true) {
-            const randomIdx = Math.floor(Math.random() * 4);
-            const selection = options[randomIdx];
+        let randomIdx, dirVector, targetTile;
 
-            if (this.grid.getTileAt(selection) === 5) {
-                continue;
-            }
+        do {
+            randomIdx = Math.floor(Math.random() * 4);
+            dirVector = options[randomIdx];
+            targetTile = this.grid.getTileAt(dirVector);
+        } while (targetTile === TILE_TYPE.BOUNDARY);
 
-            this.targetPosition = selection;
-            this.moving = true;
-            switch (randomIdx) {
-                case 0:
-                    this.velocity.set(0, -PLAYER_SPEED);
-                    return;
-                case 1:
-                    this.velocity.set(PLAYER_SPEED, 0);
-                    return;
-                case 2:
-                    this.velocity.set(0, PLAYER_SPEED);
-                    return;
-                case 3:
-                    this.velocity.set(-PLAYER_SPEED, 0);
-                    return;
-            }
+        this.targetPosition = dirVector;
+        this.moving = true;
+
+        switch (randomIdx) {
+            case 0:
+                this.velocity.set(0, -PLAYER_SPEED);
+                return;
+            case 1:
+                this.velocity.set(PLAYER_SPEED, 0);
+                return;
+            case 2:
+                this.velocity.set(0, PLAYER_SPEED);
+                return;
+            case 3:
+                this.velocity.set(-PLAYER_SPEED, 0);
+                return;
         }
-
-        // for (let i = 0, length = options.length; i < length; i++) {
-        //     if (this.grid.getTileAt(options[i]) === 5) {
-        //         continue;
-        //     }
-        //     this.targetPosition = options[i];
-
-        //     switch (i) {
-        //         case 0:
-        //             this.velocity.set(0, -PLAYER_SPEED);
-        //             return;
-        //         case 1:
-        //             this.velocity.set(PLAYER_SPEED, 0);
-        //             return;
-        //         case 2:
-        //             this.velocity.set(0, PLAYER_SPEED);
-        //             return;
-        //         case 3:
-        //             this.velocity.set(-PLAYER_SPEED, 0);
-        //             return;
-        //     }
-        // }
     }
 
     move() {
         const targetDistance = this.position.distance(this.targetPosition);
-        // const targetTile = this.grid.getTileAt(this.targetPosition);
-
-        // if (this.position.distance(this.target.position) <= this.radius) {
-        //     console.log('game over');
-        //     return;
-        // }
 
         if (targetDistance > 0) {
-
-            // if (targetTile === TILE_TYPE.BOUNDARY) {
-            //     return this.stop();
-            // }
-
             this.position.add(this.velocity);
         } else {
             this.stop();

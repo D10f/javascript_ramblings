a1a0fe1f8b65dd610b562834bca7410335f52df7
D10f <galactodensimeter_bismuthyl@simplelogin.fr>
2023-04-18 18:13:35 +0200

    Created factory function to create new enemies cyclically. Updated type definitions for character

--- 2D/pacman/src/utils/factory.ts
+++ 2D/pacman/src/utils/factory.ts
@@ -1,17 +1,49 @@
+import { CELL_SIZE, CHAR_LIST, TILE_TYPE } from '../defs';
+import CollisionComponent from '../components/CollisionComponent';
 import GraphicComponent from '../components/GraphicComponent';
+import InputComponent from '../components/InputComponent';
 import MovementComponent from '../components/MovementComponent';
 import PathGeneratorComponent from '../components/PathComponent';
 import RandomPathStrategy from '../components/RandomPathStrategy';
-import { ENEMY_LIST } from '../defs';
 import Enemy2 from "../entities/Enemy2";
+import Player2 from '../entities/Player2';
 
-export const makeEnemy = (name: keyof typeof ENEMY_LIST, x: number, y: number) => {
-    const type = ENEMY_LIST[name];
-    return new Enemy2(
+export const makeEnemyFactory = () => {
+
+    let charIdx = 0;
+    let posXIdx = 0;
+    let enemy: Character;
+
+    return () => {
+
+        do {
+            enemy = CHAR_LIST[charIdx];
+            charIdx = (charIdx + 1) % CHAR_LIST.length;
+        } while (enemy.name === 'JAVASCRIPT' || enemy.name === 'TYPESCRIPT');
+
+        const x = CELL_SIZE * 6 + (posXIdx * CELL_SIZE);
+        const y = CELL_SIZE * 8;
+
+        posXIdx = (posXIdx + 1) % 4;
+
+        return new Enemy2(
+            x,
+            y,
+            new PathGeneratorComponent(new RandomPathStrategy()),
+            new MovementComponent(),
+            new GraphicComponent(enemy.image, enemy.color)
+        );
+    };
+};
+
+export const makePlayer = (x: number, y: number) => {
+    const character = CHAR_LIST.find(c => c.name === 'JAVASCRIPT') as Character;
+    return new Player2(
         x,
         y,
-        new PathGeneratorComponent(new RandomPathStrategy()),
+        new InputComponent(),
+        new GraphicComponent(character.image, character.color),
         new MovementComponent(),
-        new GraphicComponent(new Path2D(type.image), type.color)
+        new CollisionComponent([TILE_TYPE.FOOD])
     );
 };

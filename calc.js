fd73fa0f9a1e6c2fcd71691efcbe80f3bb6bd713
D10f <galactodensimeter_bismuthyl@simplelogin.fr>
2023-09-09 14:06:47 +0200

    Just for fun

---
+++ calc.js
@@ -0,0 +1,105 @@
+const assert = require("assert");
+
+function calculate(expression) {
+  let [left, operator, ...right] = expression;
+
+  if (expression.length === 0) return 1;
+  if (expression.length === 1) return Number(left) || 1;
+
+  if (left === "(") {
+    const _expression = reduceExpression(expression);
+    return calculate([calculate(_expression[0]), ..._expression[1]]);
+  } else if (left === ")") {
+    throw new Error("Malformed expression!");
+  }
+
+  if (right[0] === "(") {
+    const _expression = reduceExpression(right);
+    right = [calculate(_expression[0]), ..._expression[1]];
+  } else if (right[0] === ")") {
+    throw new Error("Malformed expression!");
+  }
+
+  let _left;
+  let _right;
+
+  switch (operator) {
+    case "+":
+      return left + calculate(right);
+
+    case "-":
+      return left - calculate(right);
+
+    case "*":
+      _left = left * right[0];
+      _right = right.slice(1);
+      return _right.length === 0 ? _left : calculate([_left, ..._right]);
+
+    case "/":
+      _left = left / right[0];
+      _right = right.slice(1);
+      return _right.length === 0 ? _left : calculate([_left, ..._right]);
+
+    case "^":
+      _left = left ** right[0];
+      _right = right.slice(1);
+      return _right.length === 0 ? _left : calculate([_left, ..._right]);
+  }
+}
+
+function reduceExpression(expression) {
+  let parenCount = 0;
+
+  for (let i = 0; i < expression.length; i++) {
+    switch (expression[i]) {
+      case ")":
+        parenCount--;
+        break;
+      case "(":
+        parenCount++;
+        break;
+    }
+    if (parenCount === 0)
+      return [expression.slice(1, i), expression.slice(i + 1)];
+  }
+
+  throw new Error("Malformed expression!");
+}
+
+assert.throws(() => calculate(["(", 2, "+", 2]), "Error: Malformed expression");
+assert.throws(
+  () => calculate([")", 2, "+", 2]),
+  "Error: Malformed expression!",
+);
+assert.throws(() => calculate([2, "+", ")"]), "Error: Malformed expression!");
+
+assert.strictEqual(calculate([]), 1);
+assert.strictEqual(calculate(["+"]), 1);
+assert.strictEqual(calculate([23]), 23);
+assert.strictEqual(calculate([5, "+", 6]), 11);
+assert.strictEqual(calculate([5, "+", 6, "*", 8]), 53);
+assert.strictEqual(calculate([5, "*", 6, "+", 8]), 38);
+assert.strictEqual(calculate([5, "+", 6, "*", 8, "*", 2]), 101);
+assert.strictEqual(calculate([5, "*", 6, "+", 8, "*", 2]), 46);
+assert.strictEqual(calculate([5, "*", 6, "/", 8, "*", 2]), 7.5);
+assert.strictEqual(calculate([5, "*", 6, "/", "(", 8, "*", 2, ")"]), 1.875);
+assert.strictEqual(calculate([5, "*", 6, "-", 8, "/", 2]), 26);
+assert.strictEqual(calculate([5, "^", 2]), 25);
+assert.strictEqual(calculate([-20, "+", 33]), 13);
+assert.strictEqual(calculate([-20, "-", 33]), -53);
+assert.strictEqual(calculate(["(", 5, "+", 6, ")", "*", 8]), 88);
+
+assert.strictEqual(
+  calculate(["(", 5, "+", "(", 6, "+", 2, "^", 2, ")", ")", "*", 8]),
+  120,
+);
+assert.strictEqual(
+  calculate(["(", 5, "*", "(", 6, "+", 2, "^", 2, ")", ")", "*", 8]),
+  400,
+);
+assert.strictEqual(calculate([8, "*", "(", 2, "+", 2, ")"]), 32);
+assert.strictEqual(calculate([37, "*", 5]), 185);
+assert.strictEqual(calculate([37, "*", 0]), 0);
+assert.strictEqual(calculate([37, "/", 0]), Infinity);
+
+console.log("All assertions passed!");

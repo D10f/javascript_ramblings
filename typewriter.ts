ba04c5582e3fde5d0f355338e6d7e6a50809ef87
D10f <galactodensimeter_bismuthyl@simplelogin.fr>
2024-05-18 13:20:12 +0200

    Remove stop/resume functions (for now). Updated wordList to use ubuntu release codenames

--- typewriter.ts
+++ typewriter.ts
@@ -1,27 +1,77 @@
+const ubuntuCodenames = [
+  "Warty Warthog",
+  "Hoary Hedgehog",
+  "Breezy Badger",
+  "Dapper Drake",
+  "Edgy Eft",
+  "Feisty Fawn",
+  "Gutsy Gibbon",
+  "Hardy Heron",
+  "Intrepid Ibex",
+  "Jaunty Jackalope",
+  "Karmic Koala",
+  "Lucid Lynx",
+  "Maverick Meerkat",
+  "Natty Narwhal",
+  "Oneiric Ocelot",
+  "Precise Pangolin",
+  "Quantal Quetzal",
+  "Raring Ringtail",
+  "Saucy Salamander",
+  "Utopic Unicorn",
+  "Vivid Vervet",
+  "Wily Werewolf",
+  "Yakkety Yak",
+  "Zesty Zapus",
+  "Artful Aardvark",
+  "Cosmic Cuttlefish",
+  "Disco Dingo",
+  "Eoan Ermine",
+  "Groovy Gorilla",
+  "Hirsute Hippo",
+  "Impish Indri",
+  "Kinetic Kudu",
+  "Lunar Lobster",
+  "Trusty Tahr",
+  "Xenial Xerus",
+  "Bionic Beaver",
+  "Focal Fossa",
+  "Jammy Jellyfish",
+  "Mantic Minotaur",
+  "Noble Numbat",
+];
+
 type TypeWriterProps = {
   wordList: string[];
   typeSpeed: number;
-  pauseTime: number;
+  deleteSpeed: number;
+  showTextDuration: number;
+  pauseBeforeNext: number;
   elementRef: HTMLElement | string;
 };
 
 class TypeWriter {
   private wordList: string[];
   private typeSpeed: number;
-  private pauseTime: number;
+  private deleteSpeed: number;
+  private showTextDuration: number;
+  private pauseBeforeNext: number;
+  private element: HTMLElement;
   private letterIdx = 0;
   private wordIdx = 0;
-  private paused = false;
-  private element: HTMLElement;
 
   constructor({
     wordList,
     elementRef,
     typeSpeed = 100,
-    pauseTime = 1000,
+    deleteSpeed = 100,
+    showTextDuration = 1000,
+    pauseBeforeNext = 1000,
   }: TypeWriterProps) {
     this.typeSpeed = typeSpeed;
-    this.pauseTime = pauseTime;
+    this.deleteSpeed = deleteSpeed;
+    this.showTextDuration = showTextDuration;
+    this.pauseBeforeNext = pauseBeforeNext;
     this.wordList = wordList;
     this.element =
       typeof elementRef === "string"
@@ -38,17 +88,14 @@ class TypeWriter {
   }
 
   private typeKey() {
-    if (this.paused) return;
     this.element.textContent = this.word.substring(0, ++this.letterIdx);
   }
 
   private backspace() {
-    if (this.paused) return;
-    this.element.textContent = this.word.slice(0, -1);
+    this.element.textContent = this.word.substring(0, --this.letterIdx);
   }
 
   private next() {
-    if (this.paused) return;
     this.letterIdx = 0;
     this.wordIdx = (this.wordIdx + 1) % this.wordList.length;
     this.start();
@@ -56,7 +103,10 @@ class TypeWriter {
 
   start() {
     const wordLength = this.wordList[this.wordIdx].length;
-    const pauseForMs = this.typeSpeed * wordLength + this.pauseTime;
+    const showTextDuration =
+      this.typeSpeed * wordLength + this.showTextDuration;
+    const pauseBeforeNext =
+      this.deleteSpeed * wordLength + this.pauseBeforeNext;
 
     for (let i = 0; i < wordLength; i++) {
       setTimeout(this.typeKey.bind(this), this.typeSpeed * i);
@@ -64,18 +114,21 @@ class TypeWriter {
 
     setTimeout(() => {
       for (let j = wordLength; j >= 0; j--) {
-        setTimeout(this.backspace.bind(this), this.typeSpeed * j);
+        setTimeout(this.backspace.bind(this), this.deleteSpeed * j);
       }
 
-      setTimeout(this.next.bind(this), pauseForMs);
-    }, pauseForMs);
-  }
-
-  stop() {
-    this.paused = true;
-  }
-
-  resume() {
-    this.paused = false;
+      setTimeout(this.next.bind(this), pauseBeforeNext);
+    }, showTextDuration);
   }
 }
+
+const typeWriter = new TypeWriter({
+  wordList: ubuntuCodenames,
+  elementRef: "#target",
+  typeSpeed: 75,
+  deleteSpeed: 40,
+  showTextDuration: 500,
+  pauseBeforeNext: 500,
+});
+
+typeWriter.start();

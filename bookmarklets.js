d668ae84bf5f36a46faa58cfad2ae9bfbe02e706
D10f <galactodensimeter_bismuthyl@simplelogin.fr>
2024-06-10 19:25:09 +0200

    Add new bookmarklet

--- bookmarklets.js
+++ bookmarklets.js
@@ -2,12 +2,20 @@
  *  Replaces all references to Youtube, including link href attributes, and
  *  replaces them with invidious links.
  */
+javascript: (function () {
+  document.body.innerHTML = document.body.innerHTML.replace(
+    /youtu(\.)?be(.com)?/gi,
+    "invidious.snopyta.org",
+  );
+})();
 
-javascript:
-(function() {
-  document.body.innerHTML =
-  document.body.innerHTML.replace(
-    /youtu(\.)?be(.com)?/ig,
-    'invidious.snopyta.org'
-  )
-})()
+/**
+ * Opens a new tab to the search engine of choice, and search for the
+ * highlighted text on the current page.
+ */
+javascript: (function () {
+  const selection = window.getSelection().toString();
+  const searchEngine = "https://qwant.com?q=define: ";
+  const queryStr = decodeURIComponent(selection.replace(/\s+/, "+"));
+  window.open(searchEngine + queryStr, "_blank").focus();
+})();
